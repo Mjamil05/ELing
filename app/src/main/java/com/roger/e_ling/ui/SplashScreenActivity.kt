6b9601d4a7ff2a5fdb678ec771ce0f269167a52c
Mjamil05 <56086370+Mjamil05@users.noreply.github.com>
2019-12-15 03:41:43 +0700

    Initial commit

---
+++ app/src/main/java/com/roger/e_ling/ui/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package com.roger.e_ling.ui
+
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.roger.e_ling.R
+
+class SplashScreenActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
+
+    }
+}
